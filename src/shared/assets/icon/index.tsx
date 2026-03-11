de25b7bd1e10d9e2b4fd2a706507073c5ebf0398
teniryte <teniryte@gmail.com>
2026-03-11 18:46:57 +0300

    feat(ui): add ui icons (#10)

---
+++ src/shared/assets/icon/index.tsx
@@ -0,0 +1,37 @@
+import type { SVGProps } from 'react';
+
+import AddIconSvg from './add.svg?react';
+import CloseIconSvg from './close.svg?react';
+import EyeOffIconSvg from './eye-off.svg?react';
+import EyeOnIconSvg from './eye-on.svg?react';
+import LockIconSvg from './lock.svg?react';
+import MoreIconSvg from './more.svg?react';
+import PersonIconSvg from './person.svg?react';
+import PlusIconSvg from './plus.svg?react';
+import RefreshIconSvg from './refresh.svg?react';
+import SearchIconSvg from './search.svg?react';
+
+const iconsMap = {
+  add: AddIconSvg,
+  close: CloseIconSvg,
+  'eye-off': EyeOffIconSvg,
+  'eye-on': EyeOnIconSvg,
+  lock: LockIconSvg,
+  more: MoreIconSvg,
+  person: PersonIconSvg,
+  plus: PlusIconSvg,
+  refresh: RefreshIconSvg,
+  search: SearchIconSvg,
+};
+
+export type IconName = keyof typeof iconsMap;
+
+type IconProps = SVGProps<SVGSVGElement> & {
+  name: IconName;
+};
+
+export function Icon({ name, ...props }: IconProps) {
+  const Svg = iconsMap[name];
+
+  return <Svg {...props} />;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig, loadEnv } from 'vite';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import { fileURLToPath, URL } from 'node:url';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 
@@ -15,6 +16,7 @@ export default defineConfig(({ mode }) => {
         autoCodeSplitting: true,
       }),
       react(),
+      svgr(),
     ],
     resolve: {
       alias: {
